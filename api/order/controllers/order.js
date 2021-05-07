496cec94257ff0efa8728ac700c5c8b85c29061a
Leandro Lopes <leandro-lopes46@hotmail.com>
2021-05-07 20:03:20 -0300

    Add informations of card on create method of order controller

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -7,8 +7,7 @@ module.exports = {
   createPaymentIntent: async (ctx) => {
     const { cart } = ctx.request.body;
 
-    // simplify cart data
-    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart);
+    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart)
 
     // get all games
     const games = await strapi.config.functions.cart.cartItems(cartGamesIds);
@@ -20,7 +19,7 @@ module.exports = {
       };
     }
 
-    const total = await strapi.config.functions.cart.total(games);
+    const total = await strapi.config.functions.cart.total(games)
 
     if (total === 0) {
       return {
@@ -30,7 +29,7 @@ module.exports = {
 
     try {
       const paymentIntent = await stripe.paymentIntents.create({
-        amount: total * 100,
+        amount: total,
         currency: "usd",
         metadata: { cart: JSON.stringify(cartGamesIds) },
       });
@@ -42,7 +41,6 @@ module.exports = {
       };
     }
   },
-
   create: async (ctx) => {
     // pegar as informações do frontend
     const { cart, paymentIntentId, paymentMethod } = ctx.request.body;
@@ -71,20 +69,29 @@ module.exports = {
 
     // precisa pegar do frontend os valores do paymentMethod
     // e recuperar por aqui
+    let paymentInfo;
+
+    if (total_in_cents !== 0) {
+      try {
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod)
+      } catch (error) {
+        ctx.response.status = 402;
+        return { error: error.message }
+      }
+    }
+
 
     // salvar no banco
     const entry = {
       total_in_cents,
       payment_intent_id: paymentIntentId,
-      card_brand: null,
-      card_last4: null,
+      card_brand: paymentInfo?.card?.brand,
+      card_last4: paymentInfo?.card?.last4,
       user: userInfo,
       games,
     };
 
     const entity = await strapi.services.order.create(entry);
-
-    // enviar um email da compra para o usuário
 
     // retornando que foi salvo no banco
     return sanitizeEntity(entity, { model: strapi.models.order });
